3028536beff2e0899169f6a05998cf1d5bfef6b1
Varad2427 <varadbachal21@gmail.com>
2025-08-02 17:14:43 +0530

    user table created

---
+++ health_users.sql
@@ -0,0 +1,7 @@
+create database users ;
+use users;
+
+create table users(user_id int, fullname varchar(100), age int, user_role enum('citizen', 'medical_person'), address text, zone_id int, is_verified boolean, username varchar(100), password varchar (255));
+select * from users;
+desc users;
+
